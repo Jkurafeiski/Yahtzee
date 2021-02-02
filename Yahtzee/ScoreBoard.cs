ca755b0808f0d366d0fa8871321a02ea28f256ae
AMAGNO\ari <ari@amagno.de>
2021-02-02 15:28:31 +0100

    Update ScoreBoard.cs

--- Yahtzee/ScoreBoard.cs
+++ Yahtzee/ScoreBoard.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Yahtzee.Categories;
 
 namespace Yahtzee
@@ -62,41 +63,16 @@ namespace Yahtzee
             var totalScore = points;
             return totalScore;
         }
-        public int SwitchInputCalculator(int[] dice,YahtzeeCategory input)
-        {
-            switch (input)
-            {
-                case YahtzeeCategory.Sum:
-                    
-                    return new Sum().GetScore(dice);
-                case YahtzeeCategory.Pair:
-                    if (new Pair().IsMatch(dice)) return new Pair().GetScore(dice);
-                    break;
-                case YahtzeeCategory.ThreeOfAKind:
-                    if (new ThreeOfAKind().IsMatch(dice)) return new ThreeOfAKind().GetScore(dice);
-                    break;
-                case YahtzeeCategory.FourOfAKind:
-                    if (new FourOfAKind().IsMatch(dice)) return new FourOfAKind().GetScore(dice);
-                    break;
-                case YahtzeeCategory.Yahtzee:
-                    if (new Categories.Yahtzee().IsMatch(dice)) return new Categories.Yahtzee().GetScore(dice);
-                    break;
-                case YahtzeeCategory.FullHouse:
-                    if (new FullHouse().IsMatch(dice)) return new FullHouse().GetScore(dice);
-                    break;
-                case YahtzeeCategory.SmallStreet:
-                    if (new SmallStreet().IsMatch(dice)) return new SmallStreet().GetScore(dice);
-                    break;
-                case YahtzeeCategory.LargeStreet:
-                    if (new LargeStreet().IsMatch(dice)) return new LargeStreet().GetScore(dice);
-                    break;
-                case YahtzeeCategory.DoublePair:
-                    if (new DoublePair().IsMatch(dice)) return new DoublePair().GetScore(dice);
-                    break;
-            }
 
-            throw new ScoreBoardException("Das ist keine Richtige Eingabe");
+        public int SwitchInputCalculator(int[] dice, YahtzeeCategory input)
+        {
+            var res = (from category in _newscores.Keys
+                where category.YahtzeeCategory == input && category.IsMatch(dice)
+                select category.GetScore(dice)).FirstOrDefault();
+
+            return res;
         }
+
         public void PrintScoreBoard(int points)
         {
             Console.WriteLine(TotalScore(points));
